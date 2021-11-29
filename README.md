75fe6e439c12406ac84952d5fa6629b5fce9179b
LizaZaytseva <zajtseva5.ea@edu.spbstu.ru>
2021-11-29 22:34:13 +0300

    Fixed issue 2: readme + rounding

---
+++ README.md
@@ -0,0 +1,10 @@
+#Лабораторная работа №1
+В ходе данной лабораторной работы был реализован конвертор, позволяющий переводить
+значения в 3 единицы измерения: граммы, унции и караты.
+Также пользователь может регулировать точность округления значения.\
+Рассмотрим пример запуска программы: \
+```Weight converter: grams <-> carats <-> ounce``` \
+```Enter the value and specify the unit of measurement (gr,ct,oz) e.g. 123 gr: 1 oz```\
+```Enter the required unit of measurement (gr,ct,oz): ct```\
+```Rounding accuracy: 3```\
+```Result: 141.748 ct```
--- converter.py
+++ converter.py
@@ -10,14 +10,16 @@ RATIO = {
 
 def converter():
     print('Weight converter: grams <-> carats <-> ounce')
-    data = input('Enter the value and specify the unit of measurement (gr,ct,oz) e.g. 123 gr:').split()
-    unit = input('Enter the required unit of measurement (gr,ct,oz):')
+    data = input('Enter the value and specify the unit of measurement (gr,ct,oz) e.g. 123 gr: ').split()
+    unit = input('Enter the required unit of measurement (gr,ct,oz): ')
+    round_a = input('Rounding accuracy: ')
+
     ratio = data[1] + " " + unit
     if len(data) < 2 or len(unit) != 2 or ratio not in RATIO:
         print('Invalid format!')
     else:
         try:
-            print(f"Result:\t{(int(data[0]) * RATIO[ratio])} {unit}")
+            print(f"Result:\t{round(float(data[0]) * RATIO[ratio], int(round_a))} {unit}")
         except:
             print('Invalid format!')
 
